e2bfe126e6fc0b82374e47a4750311d3bb07d396
Prabhankar Tiwari <159301705+prabhankart@users.noreply.github.com>
2024-11-08 12:46:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("./db/config"); 
 require('dotenv').config();
-// Ensure your database configuration is correct
 const User = require("./db/User");
 const Product = require("./db/Product");
-const Jwt = require("jsonwebtoken");
-const jwtKey = "e-comm";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-// Middleware to verify the token
-function verifyToken(req, res, next) {
-  let token = req.headers["authorization"];
-  if (token) {
-    token = token.split(" ")[1];  // Extract the token part after "Bearer"
-    
-    Jwt.verify(token, jwtKey, (err, valid) => {
-      if (err) {
-        return res.status(401).send({ result: "Please provide a valid token" });
-      } else {
-        next();
-      }
-    });
-  } else {
-    res.status(403).send({ result: "Please add token with header" });
-  }
-}
 
 // Register route
 app.post("/register", async (req, res) => {
@@ -36,12 +15,7 @@ app.post("/register", async (req, res) => {
   let result = await user.save();
   result = result.toObject();
   delete result.password;
-  Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
-    if (err) {
-      res.send({ result: "Something went wrong..." });
-    }
-    res.send({ result, auth: token });
-  });
+  res.send(result);
 });
 
 // Login route
@@ -49,12 +23,7 @@ app.post("/login", async (req, res) => {
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-      Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
-        if (err) {
-          res.send({ result: "Something went wrong..." });
-        }
-        res.send({ user, auth: token });
-      });
+      res.send(user);
     } else {
       res.send({ result: "No user found" });
     }
@@ -63,15 +32,15 @@ app.post("/login", async (req, res) => {
   }
 });
 
-// Protected route to add a product
-app.post("/add-product", verifyToken, async (req, res) => {
+// Route to add a product
+app.post("/add-product", async (req, res) => {
   let product = new Product(req.body);
   let result = await product.save();
   res.send(result);
 });
 
-// Protected route to get all products
-app.get("/products", verifyToken, async (req, res) => {
+// Route to get all products
+app.get("/products", async (req, res) => {
   let products = await Product.find();
   if (products.length > 0) {
     res.send(products);
@@ -80,8 +49,8 @@ app.get("/products", verifyToken, async (req, res) => {
   }
 });
 
-// Protected route to delete a product by ID
-app.delete("/product/:id", verifyToken, async (req, res) => {
+// Route to delete a product by ID
+app.delete("/product/:id", async (req, res) => {
   try {
     const result = await Product.deleteOne({ _id: req.params.id });
     if (result.deletedCount > 0) {
@@ -94,8 +63,8 @@ app.delete("/product/:id", verifyToken, async (req, res) => {
   }
 });
 
-// Protected route to get a single product by ID
-app.get("/product/:id", verifyToken, async (req, res) => {
+// Route to get a single product by ID
+app.get("/product/:id", async (req, res) => {
   let result = await Product.findOne({ _id: req.params.id });
   if (result) {
     res.send(result);
@@ -104,8 +73,8 @@ app.get("/product/:id", verifyToken, async (req, res) => {
   }
 });
 
-// Protected route to update a product by ID
-app.put("/product/:id", verifyToken, async (req, res) => {
+// Route to update a product by ID
+app.put("/product/:id", async (req, res) => {
   let result = await Product.updateOne(
     { _id: req.params.id },
     { $set: req.body }
@@ -113,8 +82,8 @@ app.put("/product/:id", verifyToken, async (req, res) => {
   res.send(result);
 });
 
-// Protected search route
-app.get("/search/:key", verifyToken, async (req, res) => {
+// Search route
+app.get("/search/:key", async (req, res) => {
   let result = await Product.find({
     $or: [
       { name: { $regex: req.params.key, $options: "i" } },
@@ -124,6 +93,11 @@ app.get("/search/:key", verifyToken, async (req, res) => {
   });
   res.send(result);
 });
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running on port 5000");
+});
+
 
 app.listen(process.env.PORT||5000, () => {
   console.log("Server is running on port 5000");
